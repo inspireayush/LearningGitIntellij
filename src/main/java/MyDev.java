e501292fb66e72d3d6b56049e5e1a5e56982391a
thakurayush <ayushpratap32@gmail.com>
2022-08-15 15:25:36 +0530

    Initial Commit

---
+++ src/main/java/MyDev.java
@@ -0,0 +1,6 @@
+public class MyDev {
+
+    public static void main(String[] args) {
+        System.out.println("My dEv class");
+    }
+}
---
+++ src/test/java/MyTestClass.java
@@ -0,0 +1,6 @@
+public class MyTestClass {
+
+    public static void main(String[] args) {
+        System.out.println("My Test class");
+    }
+}
