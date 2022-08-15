34ef6cb4812c1bbc15d44aa84e33484da69195e9
thakurayush <ayushpratap32@gmail.com>
2022-08-15 15:44:03 +0530

    Commit 2

--- src/main/java/MyDev.java
+++ src/main/java/MyDev.java
@@ -2,5 +2,7 @@ public class MyDev {
 
     public static void main(String[] args) {
         System.out.println("My dEv class");
+
+        //lets do some modification now and then push it to remote repo
     }
 }
