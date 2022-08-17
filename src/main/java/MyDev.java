1209ce898f1c0af5e4daed997dd946a2db8e29e6
Ayush Tomar <111337943+inspireayush@users.noreply.github.com>
2022-08-17 17:04:19 +0530

    Update MyDev.java
    i have removed the whitespaces

--- src/main/java/MyDev.java
+++ src/main/java/MyDev.java
@@ -1,12 +1,8 @@
 public class MyDev {
-
     public static void main(String[] args) {
         System.out.println("My dEv class");
-
         //lets do some modification now and then push it to remote repo
-        
         //changed code by inspireayush 
-        
-      
+        System.out.println("No more whitespaces");
     }
 }
