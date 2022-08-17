30314b76b282780509a83febb32bf71223883230
Ayush Tomar <111337943+inspireayush@users.noreply.github.com>
2022-08-17 17:19:42 +0530

    Update MyDev.java
    further squash and merge changes

--- src/main/java/MyDev.java
+++ src/main/java/MyDev.java
@@ -4,5 +4,6 @@ public class MyDev {
         //lets do some modification now and then push it to remote repo
         //changed code by inspireayush 
         System.out.println("No more whitespaces");
+        System.out.println("Further changes to check new functionality");
     }
 }
